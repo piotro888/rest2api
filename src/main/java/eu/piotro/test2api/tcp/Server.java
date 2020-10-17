99dde7450553cf2dd3a68c78d6de43d6a5822051
Piotro <piotro888@wp.pl>
2020-10-17 14:23:58 +0200

    Faster timeout handling

--- src/main/java/eu/piotro/test2api/tcp/Server.java
+++ src/main/java/eu/piotro/test2api/tcp/Server.java
@@ -12,23 +12,26 @@ import java.util.concurrent.*;
  * @since 0.1
  */
 public class Server {
-    private static final int maxThreads = 4;
-    private static final int minKeepThreads = 1;
-    private static final int inactiveWorkerTimeout = 10;
+    private static final int defaultMaxThreads = 10;
+    private static final int defaultMinKeepThreads = 1;
+    private static final int defaultInactiveWorkerTimeout = 10;
+    private static final int defaultReadTimeout = 5000;
 
     /**
-     * Create server with custom {@link ServerExecutor} parameters
+     * Create server with custom parameters
      * @param port inet port number
      * @param forwarder APIForwarder to route HTTP requests
      * @param maxThreads maximum number of connections to handle simultaneously
      * @param minKeepThreads minimum number of handler threads to keep (more - faster reaction to traffic changes)
      * @param inactiveWorkerTimeout time in seconds to keep inactive handlers over minimum limit
+     * @param readTimeout time in milliseconds to timeout request reading
      * @throws IOException if I/O error when creating ServerSocket ex. cannot bind to port
      */
-    public Server (int port, APIForwarder forwarder, int maxThreads, int minKeepThreads, int inactiveWorkerTimeout) throws IOException {
+    public Server (int port, APIForwarder forwarder, int maxThreads, int minKeepThreads, int inactiveWorkerTimeout, int readTimeout) throws IOException {
         serverSocket = new ServerSocket(port);
         executor = new ServerExecutor(maxThreads, minKeepThreads, inactiveWorkerTimeout);
         this.forwarder = forwarder;
+        this.readTimeout = readTimeout;
     }
 
     /**
@@ -38,7 +41,7 @@ public class Server {
      * @throws IOException if I/O error when creating ServerSocket ex. cannot bind to port
      */
     public Server (int port, APIForwarder forwarder) throws IOException {
-        this(port, forwarder, maxThreads, minKeepThreads, inactiveWorkerTimeout);
+        this(port, forwarder, defaultMaxThreads, defaultMinKeepThreads, defaultInactiveWorkerTimeout, defaultReadTimeout);
     }
 
     /**
@@ -49,7 +52,7 @@ public class Server {
             Socket acceptedSocket = serverSocket.accept();
             System.out.println(acceptedSocket);
 
-            executor.execute(new ConnectionHandler(acceptedSocket, forwarder));
+            executor.execute(new ConnectionHandler(acceptedSocket, forwarder, timeoutExecutor, readTimeout));
 
         } catch (IOException e){
             e.printStackTrace();
@@ -59,4 +62,6 @@ public class Server {
     private final Executor executor;
     private final ServerSocket serverSocket;
     private final APIForwarder forwarder;
+    private final int readTimeout;
+    private final ScheduledExecutorService timeoutExecutor = Executors.newSingleThreadScheduledExecutor();
 }
